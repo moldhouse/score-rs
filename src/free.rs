2100daa48437139dddd7d647f768664feb79adae
Moritz Althaus <moritzalthaus@gmx.de>
2024-02-13 15:01:02 +0100

    chore: add Python bindings

--- src/free.rs
+++ src/free.rs
@@ -613,3 +613,56 @@ fn calculate_distance<T: Point>(points: &[T], path: &Path) -> f32 {
         .map(|(fix1, fix2)| vincenty_distance(fix1, fix2))
         .sum()
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::free;
+    use crate::free::OptimizationResult;
+    use crate::point::PointImpl;
+    use assert_approx_eq::assert_approx_eq;
+    use igc::records::BRecord;
+    use igc::util::Time;
+
+    const LEGS: usize = 6;
+
+    #[test]
+    fn free_distance() {
+        let release = Time::from_hms(8, 12, 29);
+        let result = run_free_test(include_str!("../fixtures/2023-06-17_288167.igc"), release);
+        assert_approx_eq!(result.distance, 1018.5, 0.1);
+        assert_eq!(result.path, [0, 936, 2847, 3879, 5048, 7050, 8128]);
+    }
+
+    #[test]
+    fn free_distance_with_1000m() {
+        let release = Time::from_hms(8, 16, 30);
+        let result = run_free_test(include_str!("../fixtures/schunk_1000m.igc"), release);
+        assert_approx_eq!(result.distance, 1158.61, 0.1);
+        assert_eq!(result.path, [335, 10099, 14740, 15482, 24198, 34160, 35798]);
+    }
+
+    fn run_free_test(file: &str, release: Time) -> OptimizationResult {
+        env_logger::try_init().ok();
+
+        let fixes = file
+            .lines()
+            .filter(|l| l.starts_with('B'))
+            .filter_map(|line| {
+                BRecord::parse(&line).ok().map_or(None, |record| {
+                    if record.timestamp.seconds_since_midnight() >= release.seconds_since_midnight()
+                    {
+                        Some(PointImpl {
+                            latitude: record.pos.lat.into(),
+                            longitude: record.pos.lon.into(),
+                            altitude: record.pressure_alt,
+                        })
+                    } else {
+                        None
+                    }
+                })
+            })
+            .collect::<Vec<_>>();
+
+        free::optimize(&fixes, 0.0, LEGS).unwrap()
+    }
+}
